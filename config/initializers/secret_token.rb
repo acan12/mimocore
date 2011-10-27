2e40752534c512bcf5da5f2a96e87edac792c920
aryx <arycancer@gmail.com>
2011-10-27 12:32:42 +0700

    upload mimocore first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mimocore::Application.config.secret_token = 'ad442c4c3a3f037e230b76a6e89e4659d35e0983f26d784e5f13783239dc34f3c2b34d74915d768695622050eea072cfe93e35be3538e8fd337282fbd43f4188'
