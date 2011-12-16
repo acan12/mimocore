e6704dc01808e5b85081f73dd06090a86ae2a6e6
aryx <arycancer@gmail.com>
2011-12-16 18:07:33 +0700

    disable connection to db

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,15 +1,8 @@
-MongoMapper.config = { 
-  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
-                          'mongodb://localhost/app1611555' } }
+MongoMapper.connection = Mongo::Connection.new('127.0.0.1')
+MongoMapper.database = "mimodb"
 
-MongoMapper.connect(Rails.env)
-
-##MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
-#MongoMapper.database = "app1611555"
-##MongoMapper.database = "mimodb"    
-
-##if defined?(PhusionPassenger)
-##  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-##   MongoMapper.connection.connect_to_master if forked
-##  end
-##end
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+   MongoMapper.connection.connect_to_master if forked
+  end
+end
--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,8 +1,21 @@
-MongoMapper.connection = Mongo::Connection.new('127.0.0.1')
-MongoMapper.database = "mimodb"
+                             
 
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-   MongoMapper.connection.connect_to_master if forked
-  end
-end
+
+
+#MongoMapper.config = { 
+#  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+#                          'mongodb://localhost/app1611555' } }
+
+#MongoMapper.connect(Rails.env)
+ 
+
+
+##MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
+#MongoMapper.database = "app1611555"
+##MongoMapper.database = "mimodb"    
+
+##if defined?(PhusionPassenger)
+##  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+##   MongoMapper.connection.connect_to_master if forked
+##  end
+##end
