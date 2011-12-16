9c82fa3a006d35bd0b5225cc46df868ad33171e6
aryx <arycancer@gmail.com>
2011-12-16 14:19:16 +0700

    test connection mongodb 1

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,15 +1,15 @@
-MongoMapper.config = { 
-  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
-                          'mongodb://localhost/sushi' } }
+#MongoMapper.config = { 
+#  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+#                          'mongodb://localhost/sushi' } }
 
-MongoMapper.connect(Rails.env)
+#MongoMapper.connect(Rails.env)
 
-##MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
-##MongoMapper.database = "app1611555"
-#MongoMapper.database = "mimodb"    
+MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
+#MongoMapper.database = "app1611555"
+MongoMapper.database = "mimodb"    
 
-##if defined?(PhusionPassenger)
-##  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-##   MongoMapper.connection.connect_to_master if forked
-##  end
-##end
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+   MongoMapper.connection.connect_to_master if forked
+  end
+end
