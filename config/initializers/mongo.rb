b8abd1acbf8f4e8bbea20c3ade998fe7a7492704
aryx <arycancer@gmail.com>
2011-12-16 18:32:10 +0700

    disable db connection #2

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,15 @@
-MongoMapper.connection = Mongo::Connection.new("mongodb://heroku:41a14a0d9537e26db2d916bf71141504@staff.mongohq.com:10037/app1611555")
-#MongoMapper.database = "app1611555"
+#MongoMapper.config = { 
+#  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+#                          'mongodb://localhost/app1611555' } }
 
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-   MongoMapper.connection.connect_to_master if forked
-  end
-end
+#MongoMapper.connect(Rails.env)
+
+##MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
+#MongoMapper.database = "app1611555"
+##MongoMapper.database = "mimodb"    
+
+##if defined?(PhusionPassenger)
+##  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+##   MongoMapper.connection.connect_to_master if forked
+##  end
+##end
