b1530db9b21a67661e772f63b0a6b13b534a4d47
aryx <arycancer@gmail.com>
2011-12-17 10:27:43 +0700

    add configuration mongodb

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,12 +1,12 @@
-MongoMapper.config = { 
-  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
-                          'mongodb://heroku:41a14a0d9537e26db2d916bf71141504@staff.mongohq.com:10037/app1611555' } }
+#MongoMapper.config = { 
+#  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+#                          'mongodb://localhost/app1611555' } }
 
-MongoMapper.connect(Rails.env)
+#MongoMapper.connect(Rails.env)
 
-#MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
-#MongoMapper.database = "app1611555"
-##MongoMapper.database = "mimodb"    
+MongoMapper.connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])             
+MongoMapper.database = "app1611555"
+#MongoMapper.database = "mimodb"    
 
 ##if defined?(PhusionPassenger)
 ##  PhusionPassenger.on_event(:starting_worker_process) do |forked|
