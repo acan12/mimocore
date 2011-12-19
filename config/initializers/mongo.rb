889e9215b6d0056dbac1c970db1352857a5feab1
aryx <arycancer@gmail.com>
2011-12-19 13:21:08 +0700

    test config database

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -4,12 +4,12 @@
 
 #MongoMapper.connect(Rails.env)
 
-MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
+#MongoMapper.connection = Mongo::Connection.new('127.0.0.1')             
 #MongoMapper.database = "app1611555"
-MongoMapper.database = "mimodb"    
+#MongoMapper.database = "mimodb"    
 
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-   MongoMapper.connection.connect_to_master if forked
-  end
-end
+#if defined?(PhusionPassenger)
+#  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+#   MongoMapper.connection.connect_to_master if forked
+#  end
+#end
