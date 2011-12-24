78072a72aec36320ac24fcd55005bd501b9b3ed3
aryx <arycancer@gmail.com>
2011-12-24 17:00:39 +0700

    return just one record

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   respond_to :json
   
   def index
-    @comments = Comments.all
+    @comments = Comments.all[0]
     respond_with(@comments)
   end
 end
