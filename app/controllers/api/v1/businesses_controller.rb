b19182856b215ed2e2076e65348fb7f47813dbde
aryx <arycancer@gmail.com>
2012-02-19 17:51:43 +0700

    pass new parameter default 'created'

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BusinessesController < Api::V1::ApplicationController
   def index
-    @business = Businesses.all
+    @business = Businesses.order_by([:created, :desc])
     respond_with(@business)
   end                      
   
@@ -9,7 +9,7 @@ class Api::V1::BusinessesController < Api::V1::ApplicationController
      bizname = params[:bizname]
      desc  = params[:description]
                                                    
-     @business = Businesses.create(:bizname => bizname, :category => "market", :description => desc)
+     @business = Businesses.create(:bizname => bizname, :category => "market", :description => desc, :created => Time.now)
      
      respond_with(@business, :location => api_v1_biz_url)         
   end
