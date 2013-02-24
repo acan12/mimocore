2462b0e002bd1a8d0d5aaf7f200940b055bb4160
arycancer <arycancer@gmail.com>
2013-02-24 19:15:46 +0700

    fix deploy to heroku

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::BusinessesController < Api::V1::ApplicationController             
+class Api::V1::BusinessesController < Api::V1::ApiApplicationController             
   respond_to :json
   
   
